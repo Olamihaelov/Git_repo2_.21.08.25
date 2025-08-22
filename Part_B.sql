705ed0a30c9da795a6615016ab5439853c2abb10
Olamihaelov <olamihaelov@gmail.com>
2025-08-22 11:10:45 +0300

    Update Part_B.sql

--- Part_B.sql
+++ Part_B.sql
@@ -1,6 +1,20 @@
 GROUP BY Queries:
 
   1
-select count(genre)
-from movies;
+SELECT genre, COUNT(*) as movies_count
+FROM movies
+GROUP BY genre
+ORDER BY movies_count DESC;
 
+  2
+SELECT SUM(revenue) as all_revenue , year
+FROM movies
+GROUP BY year
+
+  3
+SELECT genre, AVG(revenue) as avg_revenue
+FROM movies
+GROUP BY genre
+ORDER BY avg_revenue DESC;
+
+  4
