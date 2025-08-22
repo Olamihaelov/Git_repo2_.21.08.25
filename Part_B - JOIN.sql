8ef3dab6557d79cb434f681a4632adb99ab53aaf
Olamihaelov <olamihaelov@gmail.com>
2025-08-22 16:37:35 +0300

    Update Part_B - JOIN.sql

--- Part_B - JOIN.sql
+++ Part_B - JOIN.sql
@@ -1,1 +1,16 @@
 JOIN Queries:
+
+  1
+SELECT t.first_name, t.last_name, c.country_name
+FROM tourists t
+INNER JOIN countries c ON c.id = t.country_id;
+
+  2
+SELECT t.first_name, t.last_name, tr.*
+FROM tourists t
+INNER JOIN tours tr ON tr.id = t.tour_id;
+
+  3
+SELECT t.first_name, t.last_name, tr.*
+FROM tourists t
+left JOIN tours tr ON tr.id = t.tour_id;
