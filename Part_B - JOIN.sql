a44c775e610adc20d758e4c28906a4d95f28f40c
Olamihaelov <olamihaelov@gmail.com>
2025-08-22 17:19:14 +0300

    Update Part_B - JOIN.sql

--- Part_B - JOIN.sql
+++ Part_B - JOIN.sql
@@ -14,3 +14,46 @@ INNER JOIN tours tr ON tr.id = t.tour_id;
 SELECT t.first_name, t.last_name, tr.*
 FROM tourists t
 left JOIN tours tr ON tr.id = t.tour_id;
+
+  4
+SELECT
+    t.id AS tourist_id,
+    t.first_name,
+    t.last_name,
+    t.passport_number,
+    t.country_id,
+    tr.id AS tour_id,
+    tr.tour_name,
+    tr.description,
+    tr.start_date,
+    tr.end_date,
+    tr.price,
+    tr.max_participants,
+    tr.guide_name,
+    tr.difficulty_level,
+    tr.pickup_location
+FROM tourists  AS t
+LEFT JOIN tours AS tr ON t.tour_id = tr.id
+
+UNION
+
+SELECT
+    t.id,
+    t.first_name,
+    t.last_name,
+    t.passport_number,
+    t.country_id,
+    tr.id,
+    tr.tour_name,
+    tr.description,
+    tr.start_date,
+    tr.end_date,
+    tr.price,
+    tr.max_participants,
+    tr.guide_name,
+    tr.difficulty_level,
+    tr.pickup_location
+FROM tours AS tr
+LEFT JOIN tourists AS t ON t.tour_id = tr.id
+WHERE t.id IS NULL;
+
