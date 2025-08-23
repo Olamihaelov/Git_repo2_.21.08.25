bf5ee8ae845415e735f88af9d562b99582a55275
Olamihaelov <olamihaelov@gmail.com>
2025-08-23 10:30:56 +0300

    Update Part_D – Bonus (2).sql

--- Part_D – Bonus (2).sql
+++ Part_D – Bonus (2).sql
@@ -1,3 +1,14 @@
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    movie_name TEXT NOT NULL UNIQUE,
+    genre TEXT NOT NULL,
+    country TEXT NOT NULL,
+    language TEXT NOT NULL,
+    year INTEGER NOT NULL CHECK (year >= 2009),
+    revenue REAL NOT NULL CHECK (revenue >= 0)
+);
+
+
 DROP FUNCTION IF EXISTS total_revenue_by_year(INTEGER);
 
 CREATE OR REPLACE FUNCTION total_revenue_by_year(p_year INTEGER)
